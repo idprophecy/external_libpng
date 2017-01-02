bb42deab99fcc8349434b838a9b6bf49fa03976d
mydongistiny <jaysonedson@gmail.com>
2017-01-01 19:51:32 -0800

    libpng: fix linker error with zlib 1.2.9
    don't need the define anyways since we won't be using the removed version
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- pngrutil.c
+++ pngrutil.c
@@ -452,13 +452,6 @@ png_inflate_claim(png_structrp png_ptr, png_uint_32 owner)
             png_ptr->flags |= PNG_FLAG_ZSTREAM_INITIALIZED;
       }
 
-#if ZLIB_VERNUM >= 0x1282
-   defined(PNG_SET_OPTION_SUPPORTED) && defined(PNG_IGNORE_ADLER32)
-      if (((png_ptr->options >> PNG_IGNORE_ADLER32) & 3) == PNG_OPTION_ON)
-         /* Turn off validation of the ADLER32 checksum in IDAT chunks */
-         ret = inflateValidate(&png_ptr->zstream, 0);
-#endif
-
       if (ret == Z_OK)
          png_ptr->zowner = owner;
 
